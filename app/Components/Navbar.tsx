16045d74d7068f755ee4f346e2ab226881837031
Mohfazam <mohd.sarwar.code@gmail.com>
2025-07-21 17:04:26 +0530

    Hero Section 80%

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -3,7 +3,7 @@ export const Navbar = () => {
         <div className="mt-[22px] ml-[55px] flex justify-center bg-transparent">
             <div className="flex gap w-[1345px] h-[62px] gap-[32px]">
                 <div className="w-[193px] h-[63px] ">
-                    <img src="./main_Logo.png" alt="Reducate Logo" />
+                    <img src="./Main_Logo.png" alt="Reducate Logo" />
                 </div>
 
                 <div className="flex justify-center items-center gap-[86px] font-400 text-[#FFFFFF]">
