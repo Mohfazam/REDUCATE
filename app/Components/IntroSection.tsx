806f119339f812f442e67ea40777119b91becc58
Mohfazam <mohd.sarwar.code@gmail.com>
2025-07-22 00:44:37 +0530

    DualCard Init

--- app/Components/IntroSection.tsx
+++ app/Components/IntroSection.tsx
@@ -1,9 +1,12 @@
 import { CardV1 } from "../UI/CardV1"
+import { DualCard } from "../UI/DualCard"
 
 export const IntroSection = () => {
     return(
-        <div className="flex justify-between w-full h-[780px] bg-[#000E1C]">
+        <div className="flex justify-center w-full h-[780px] bg-[#000E1C] ">
             <CardV1 />
+
+            <DualCard />
         </div>
     )
 }
---
+++ app/UI/DualCard.tsx
@@ -0,0 +1,7 @@
+export const DualCard = () => {
+    return(
+        <div className="w-[877px] h-[659px] rounded-md text-white bg-gradient-to-b from-[#17213D] to-[#000D1C] m-4 py-2">
+                ewncj
+        </div>
+    )
+}
