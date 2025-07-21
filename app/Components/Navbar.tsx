7666b04ffb2528ece466c253c11e5e5b0579a377
Mohfazam <mohd.sarwar.code@gmail.com>
2025-07-21 10:50:40 +0530

    navbar

---
+++ app/Components/Navbar.tsx
@@ -0,0 +1,37 @@
+export const Navbar = () => {
+    return (
+        <div className="mt-[22px] ml-[55px] flex justify-center">
+            <div className="flex gap w-[1345px] h-[62px]">
+                <div className="w-[193px] h-[63px] gap-[32px]">
+                    Logo
+                </div>
+
+                <div className="flex gap-[86px]">
+                    <div>
+                    For Schools
+                </div>
+
+                <div>
+                    For Colleges
+                </div>
+
+                <div>
+                    For Franchisees
+                </div>
+
+                <div>
+                    For Corporate
+                </div>
+
+                <div>
+                    Out Team
+                </div>
+
+                <div>
+                    Contact Us
+                </div>
+                </div>
+            </div>
+        </div>
+    )
+}
