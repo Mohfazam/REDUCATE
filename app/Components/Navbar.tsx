6de3839166726f1c25bdb2c4cf8c9b7ee01608d0
Mohfazam <mohd.sarwar.code@gmail.com>
2025-07-23 01:27:22 +0530

    Navbar Responsive

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -1,12 +1,16 @@
 export const Navbar = () => {
     return (
         <div className="w-full flex justify-center bg-transparent">
-            <div className="flex gap w-full h-[62px] gap-[32px]">
+            <div className="flex w-full h-[62px] gap-[32px] sm:justify-between ml-[42px] mr-[42px] lg:ml-0 lg:mr-0 lg:justify-center">
                 <div className="w-[193px] h-[63px] ">
                     <img src="./Main_Logo.png" alt="Reducate Logo"  draggable="false"/>
                 </div>
 
-                <div className="flex w-full justify-center items-center gap-[39px] font-400 text-[#FFFFFF] text-[24px]">
+                <div className="block lg:hidden text-white text-[30px] mt-3">
+                     ☰
+                </div>
+
+                <div className="hidden lg:flex w-full justify-center items-center gap-[39px] font-400 text-[#FFFFFF] text-[24px]">
                     <div className="w-fit [h-27px]">
                         For Schools
                     </div>
@@ -31,6 +35,8 @@ export const Navbar = () => {
                         Contact Us
                     </div>
                 </div>
+
+                
             </div>
         </div>
     )
